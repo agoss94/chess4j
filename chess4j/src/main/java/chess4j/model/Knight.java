8971eb6265a054741329aa1e3b7194cda507495d
Andreas <andreas.goss1994@gmail.com>
2022-04-11 11:48:15 +0200

    Add Knight

---
+++ chess4j/src/main/java/chess4j/model/Knight.java
@@ -0,0 +1,23 @@
+package chess4j.model;
+
+public class Knight extends AbstractPiece implements Piece {
+
+	protected Knight(Color color) {
+		super(Type.KNIGHT, color);
+	}
+
+	public static Knight white() {
+		return new Knight(Color.WHITE);
+	}
+
+	public static Knight black() {
+		return new Knight(Color.BLACK);
+	}
+
+	@Override
+	public boolean isValidMove(Position start, Position end) {
+		int dirX = end.getColumn() - start.getColumn();
+		int dirY = end.getRow() - start.getRow();
+		return (Math.abs(dirX) == 2 && Math.abs(dirY) == 1) || (Math.abs(dirX) == 1 && Math.abs(dirY) == 2);
+	}
+}
