b61073457c35e99aafd9e6b3801763b6b583f335
Andreas <andreas.goss1994@gmail.com>
2022-04-11 10:58:42 +0200

    Add color and Piece

---
+++ chess4j/src/main/java/chess4j/model/Color.java
@@ -0,0 +1,14 @@
+package chess4j.model;
+
+public enum Color {
+	/**
+	 * The color white.
+	 */
+	WHITE,
+
+	/**
+	 * The color black.
+	 */
+	BLACK;
+
+}
--- chess4j/src/main/java/chess4j/model/Piece.java
+++ chess4j/src/main/java/chess4j/model/Piece.java
@@ -2,4 +2,50 @@ package chess4j.model;
 
 public interface Piece {
 
+	/**
+	 * Get the type of the piece
+	 *
+	 * @return the type of the piece.
+	 */
+	Type getType();
+
+	/**
+	 * Get the color of the piece.
+	 *
+	 * @return the color of the piece.
+	 */
+	Color getColor();
+
+	/**
+	 * Determines if the move is valid for the piece.
+	 *
+	 * @param start position of the piece at the beginning.
+	 * @param end   position of the piece at the end.
+	 * @return true is the move is valid false otherwise.
+	 */
+	boolean isValidMove(Position start, Position end);
+
+	/**
+	 * Determines if the capture is valid for the piece.
+	 *
+	 * @param start position of the piece at the beginning.
+	 * @param end   position of the piece at the end.
+	 * @return true is the move is valid false otherwise.
+	 */
+	boolean isValidCapture(Position start, Position end);
+
+	enum Type {
+
+		PAWN,
+
+		ROOK,
+
+		KNIGHT,
+
+		BISHOP,
+
+		QUEEN,
+
+		KING;
+	}
 }
