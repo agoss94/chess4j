db34bbbcec6d28029be1776fa5047ec0a2f6290f
Andreas Goss <agoss@itemis.com>
2024-01-12 11:41:19 +0100

    Remove unused class from interface, which is part of utilities class

--- src/main/java/org/chess4j/Board.java
+++ src/main/java/org/chess4j/Board.java
@@ -9,48 +9,4 @@ import java.util.function.Predicate;
 
 public interface Board extends Map<Tile, Piece> {
 
-    /**
-     * A private implementation of a Board that is backed by a given board. The
-     * returned instance is unmodifiable. All pieces
-     */
-     static class FilteredBoard extends AbstractMap<Tile, Piece> implements Board {
-
-        /**
-         * The filter condition.
-         */
-        private final Predicate<Piece> condition;
-
-        /**
-         * The entry set will be updated with the original map.
-         */
-        private final Board board;
-
-        /**
-         * Private constructor
-         *
-         * @param condition the predicate which is used for filtering.
-         */
-        private FilteredBoard(Board board, Predicate<Piece> condition) {
-            this.condition = condition;
-            this.board = board;
-        }
-
-        @Override
-        public Set<Entry<Tile, Piece>> entrySet() {
-            return new AbstractSet<Entry<Tile, Piece>>() {
-
-                @Override
-                public Iterator<Entry<Tile, Piece>> iterator() {
-                    return board.entrySet().stream().filter(e -> condition.test(e.getValue())).iterator();
-                }
-
-                @Override
-                public int size() {
-                    return (int) board.entrySet().stream().filter(e -> condition.test(e.getValue())).count();
-                }
-            };
-        }
-
-    }
-
 }
